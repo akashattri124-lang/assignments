bda2d2541eb3127053f8f6834ca2eddbb5c22633
akashattri124-lang <akashattri124@gmail.com>
2026-01-10 00:05:22 +0530

    solved anagram problem

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+  if( str1.length != str2.length){
+    return false;
 
 }
+  const freq = {};
+  for( let ch of str1){
+    freq[ch] = (freq[ch] || 0) +1;
+  }
+  for( let ch of str2){
+    if( !freq[ch]{
+      return false;
+    }
+    freq[ch]--;
+  }
+  return true;
 
 module.exports = isAnagram;
